81d7a60e532c15c60ec4b7b365455041bd790a78
Gary Pang <gary.pang@gmail.com>
2016-03-04 02:57:23 -0500

    Create data models
    Add two Mongoose data models ("Comments" and "Posts").

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index'),
+    users = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose');
+
+// Register Mongoose data models
+require('./models/Posts');
+require('./models/Comments');
+
+// Conect to local MongoDB instance
+mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

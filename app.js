03524927c0ffcd6d1ba6f365db0121007ec0d8f6
Gary Pang <gary.pang@gmail.com>
2016-03-04 01:09:54 -0500

    Inject ui-router
    Create "home" and "posts" ui-states. Create templates for ui-views. Add
    PostsCtrl to app.js.

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui-router']);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function(){
 	var o = {
@@ -11,7 +31,6 @@ app.controller('MainCtrl', [
 	'$scope',
 	'posts',
 	function($scope, posts){
-		$scope.test = 'Hello world!';
 		$scope.posts = posts.posts;
 
 		// function that adds object into posts array
@@ -23,7 +42,11 @@ app.controller('MainCtrl', [
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}					
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
@@ -33,4 +56,23 @@ app.controller('MainCtrl', [
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
 		};
+}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function() {
+			if ($scope.body === '') {return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+		};
+
 }]);

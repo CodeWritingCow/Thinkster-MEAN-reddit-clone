c734fd763a085a010d1a47ad15e318f3fc5e5b99
Gary Pang <gary.pang@gmail.com>
2016-03-06 22:10:15 -0500

    Add routes
    Add GET route for retrieving posts, and POST route for making new posts.

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+	mongoose = require('mongoose'),
+	Post = mongoose.model('Post'),
+	Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET posts*/
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if(err) {
+			return next(err);
+		}
+		res.json(posts);
+	});
+});
+
+/* POST Flapper News posts*/
+router.post('/posts', function(req, res, next) {
+	var post = new Post(req.body);
+
+	post.save(function(err, post) {
+		if (err) {
+			return next(err);
+		}
+		res.json(post);
+	});
+});
+
+
 
 module.exports = router;

39432f887ed10fea43a8167cb2cc95673d645f52
Gary Pang <gary.pang@gmail.com>
2016-03-12 02:53:29 -0500

    Add route
    Add POST route for authenticating users.

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -118,8 +118,25 @@ router.post('/register', function(req, res, next) {
 	user.save(function(err) {
 		if (err) { return next(err); }
 
-		return res.json({ token: user.generateJWT()})
+		return res.json({ token: user.generateJWT()});
 	});
+});
+
+/* POST route for authenticating users */
+router.post('/login', function(req, res, next) {
+	if (!req.body.username || !req.body.password) {
+		return res.status(400).json({ message: 'Please fill out all fields' });
+	}
+
+	passport.authenticate('local', function(err, user, info) {
+		if (err) { return next(err); }
+
+		if (user) {
+			return res.json({ token: user.generateJWT() });
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res, next);
 });
 
 module.exports = router;

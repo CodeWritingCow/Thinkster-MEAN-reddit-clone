d3ec9456e0a4017f9423499a66f00b84dc4145dd
Gary Pang <gary.pang@gmail.com>
2016-03-16 02:56:27 -0400

    Update gulpfile.js
    Gulp now checks JavaScript files for errors.

--- flapper-news/gulpfile.js
+++ flapper-news/gulpfile.js
@@ -1,7 +1,8 @@
 // load gulp plugins
 var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	jshint = require('gulp-jshint');
 
 // define task called css
 gulp.task('css', function() {
@@ -11,4 +12,12 @@ gulp.task('css', function() {
 		.pipe(minifyCSS())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('public/assets/css'));
+});
+
+// task for linting js files
+gulp.task('js', function() {
+	
+	return gulp.src(['public/assets/js/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
 });

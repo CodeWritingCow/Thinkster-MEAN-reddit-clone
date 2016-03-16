629847fce14255bcb097f957e3d3ce5c219e107f
Gary Pang <gary.pang@gmail.com>
2016-03-16 02:34:38 -0400

    Update gulpfile.js
    Gulp now minifies CSS files and saves them as new .min.css files.

--- flapper-news/gulpfile.js
+++ flapper-news/gulpfile.js
@@ -1,7 +1,14 @@
+// load gulp plugins
 var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
 	rename = require('gulp-rename');
 
-gulp.task('css'), function() {
-	// Gulp magic will go here!
-};
+// define task called css
+gulp.task('css', function() {
+
+	// grab css file, minify it, save to style.min.css
+	return gulp.src('public/assets/css/style.css')
+		.pipe(minifyCSS())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('public/assets/css'));
+});

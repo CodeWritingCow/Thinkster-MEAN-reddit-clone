228f28122618e222f1ca8af7d050d681ced939b5
Gary Pang <gary.pang@gmail.com>
2016-03-17 03:31:43 -0400

    Create postsService
    Split postsService from angularApp.js.

--- flapper-news/public/app/angularApp.js
+++ flapper-news/public/app/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router', 'navCtrl', 'authCtrl', 'authService', 'postsCtrl', 'mainCtrl']);
+var app = angular.module('flapperNews', ['ui.router', 'navCtrl', 'authCtrl', 'authService', 'postsCtrl', 'postsService', 'mainCtrl']);
 
 app.config([
 	'$stateProvider',
@@ -48,59 +48,4 @@ app.config([
 			});
 
 		$urlRouterProvider.otherwise('home');
-}]);
-
-
-
-
-app.factory('posts', ['$http', 'auth', function($http, auth){
-	var o = {
-		posts: []
-	};
-
-	o.getAll = function() {
-		return $http.get('/posts').success(function(data) {
-			angular.copy(data, o.posts);
-		});
-	};
-
-	o.create = function(post) {
-		return $http.post('/posts', post, {
-			headers: { Authorization: 'Bearer ' + auth.getToken() }
-		}).success(function(data) {
-			o.posts.push(data);
-		});
-	};
-
-	o.upvote = function(post) {
-		return $http.put('/posts/' + post._id + '/upvote', null, {
-			headers: { Authorization: 'Bearer ' + auth.getToken() }
-		})
-		.success(function(data) {
-			post.upvotes += 1;
-		});
-	};
-
-	o.get = function(id) {
-		return $http.get('/posts/' + id).then(function(res) {
-			return res.data;
-		});
-	};
-
-	o.addComment = function(id, comment) {
-		return $http.post('/posts/' + id + '/comments', comment, {
-			headers: { Authorization: 'Bearer ' + auth.getToken() }
-		});
-	};
-
-	o.upvoteComment = function(post, comment) {
-		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
-			headers: { Authorization: 'Bearer ' + auth.getToken() }
-		}).success(function(data) {
-			comment.upvotes +=1;
-		});
-	};
-
-	return o;
-
 }]);

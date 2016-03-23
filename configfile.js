50f4d3b4f5ec4d56e741bb226f130fe441b7058f
Gary Pang <gary.pang@gmail.com>
2016-03-23 04:01:51 -0400

    Fix typo
    Add missing second period to process.env.PORT. Could this be causing all
    these problems?! Also set config var "NODE_ENV=production" via Heroku
    command line as recommended by a tutorial at
    http://nieldlr.com/2012/03/the-really-absolute-beginners-guide-to-host-node-js-on-heroku/

--- configfile.js
+++ configfile.js
@@ -2,7 +2,7 @@
 // manage app settings here (port number, database connection, etc.
 
 module.exports = {
-	'port': process.envPORT || 3000, // set port for the app
+	'port': process.env.PORT || 3000, // set port for the app
 	'database': process.env.MONGOOSE_DATABASE, // secret link to secret database! Stored as an environmental variable.
 	// formerly 'database': 'mongodb://localhost/news',
 	'secret': process.env.SECRET_KEY // IT'S A SECRET!
